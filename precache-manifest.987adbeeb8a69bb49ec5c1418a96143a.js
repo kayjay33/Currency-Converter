dc2c5f25ea131d5d333223ffbe8f899f1759df2d
Kayjay <cascadecustomer@Kayjays-Macbook.local>
2021-02-06 15:02:34 -0800

    Updates

---
+++ precache-manifest.987adbeeb8a69bb49ec5c1418a96143a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bc16e23c3d55e4816f9b224a44c21d",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "1c6a795fe049912c5581",
+    "url": "/currency-converter/static/css/main.df32177d.chunk.css"
+  },
+  {
+    "revision": "82dcf2b234778fb84a2b",
+    "url": "/currency-converter/static/js/2.04480cee.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/currency-converter/static/js/2.04480cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6a795fe049912c5581",
+    "url": "/currency-converter/static/js/main.edee0e04.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  }
+]);
